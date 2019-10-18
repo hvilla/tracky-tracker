0338107fc1cb75e4f4344c25667bc7280b9c1a76
Héctor Villa <hectorpizarro1989@gmail.com>
2019-10-18 07:52:32 -0500

    Fixed dependencies

--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ mongoose.Promise = global.Promise;
 //PORT FOR APP
 const port = process.env.APP_PORT || 9000;
 
-const mongodb = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+//const mongodb = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
+const mongodb = 'mongodb+srv://ttpuser:fElG84JrS5o5aI6F@cluster0-cvu20.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(mongodb,{useNewUrlParser: true ,useUnifiedTopology: true,useCreateIndex:true})
 .then(() => {  
     console.log("Connected to db succesfully")
